00d12ef06305ad798bfb0ee05f4d848ad00df341
Jason-Delancey <jason.r.delancey@gmail.com>
2015-10-29 12:58:22 -0400

    Completed Exercise P1.2

---
+++ BigJava_V3_Chapter1/src/FacePrinter.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+
+/**
+ * Prints a face using text characters
+ * @author CloudStrife
+ *
+ */
+public class FacePrinter
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		/* Create the hair */
+		System.out.println("  /////  ");
+		
+		/* Create the eyes */
+		System.out.println(" | o o | ");
+		
+		/* Create the nose */
+		System.out.println("(|  ^  |)");
+		
+		/* Create the mouth */
+		System.out.println(" | \\_/ |");
+		
+		/* Create the chin */
+		System.out.println("  _____  ");
+		
+	}
+
+}
