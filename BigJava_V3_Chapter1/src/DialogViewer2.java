72a2c74acfd6b7a5400c7285447350fc3836045b
Jason-Delancey <jason.r.delancey@gmail.com>
2015-11-12 10:08:27 -0500

    Completed Exercise P1.7

---
+++ BigJava_V3_Chapter1/src/DialogViewer2.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+/**
+ * 
+ */
+
+/**
+ * Modified DialogViewer class that shows the message "Hello, your name!"
+ * @author CloudStrife
+ *
+ */
+public class DialogViewer2
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		JOptionPane.showMessageDialog(null, "Hello, Jason!");
+		System.exit(0);
+		// TODO Auto-generated method stub
+
+	}
+
+}
